204427b7e21c24f8049eb4216609944a93dc4a68
Madhukiran00 <162177926+Madhukiran00@users.noreply.github.com>
2025-03-28 22:28:21 +0530

    Add files via upload

---
+++ 28-3-Task1.py
@@ -0,0 +1,65 @@
+def add(a,b):
+     return a+b
+ 
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+ 
+def div(a,b):
+    return a/b
+
+def mod_div(a,b):
+    return a%b
+    
+def flo_div(a,b):
+    return a//b
+
+def pow(a,b):
+    return a**b
+
+add1=lambda a,b:a+b
+
+sub1=lambda a,b:a-b
+
+mul1=lambda a,b:a*b
+
+div1=lambda a,b:a/b
+
+mod_div1=lambda a,b:a%b
+
+flo_div1=lambda a,b:a//b
+
+pow1=lambda a,b:a**b
+
+
+
+
+print(add(2,4))
+
+print(sub(2,4))
+
+print(div(2,4))
+
+print(mod_div(2,4))
+
+print(flo_div(2,4))
+
+print(pow(2,4))
+
+
+print("          ")
+
+
+print(add1(2,4))
+
+print(sub1(2,4))
+
+print(div1(2,4))
+
+print(mod_div1(2,4))
+
+print(flo_div1(2,4))
+
+print(pow1(2,4))
